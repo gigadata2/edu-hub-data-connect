ae05d5e4b01a03778576bfd1ef6dd1a23b0ef540
gigadata2 <datagiga0@gmail.com>
2025-12-12 22:01:01 +0000

    Fix component structure by removing unnecessary div and cleaning up export statement

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -426,12 +426,8 @@ export default function Transactions() {
           </TabsContent>
         </Tabs>
       </main>
-<<<<<<< HEAD
     );
   }
-=======
-    </div>
-  );
-}
->>>>>>> e213daa1b7e8a8e58369b4e2ff7b60763c6d39c3
+  
+  export default Transactions;
 
